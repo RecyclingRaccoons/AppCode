9112629daf47d4936740437a4a65e577c4423df2
aswu308 <aswu@wpi.edu>
2021-04-15 08:52:06 -0400

    Version 2

--- app/src/main/java/com/example/myapplication/RecyclingImportance.java
+++ app/src/main/java/com/example/myapplication/RecyclingImportance.java
@@ -12,21 +12,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RecyclingImportance extends AppCompatActivity {
 
-    /*ViewGroup tContainer;
+    ViewGroup tContainer;
     TextView txt;
-    Button magic;*/
+    Button magic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycling_importance);
-    }
 
-    public void recyclingImportance(View v) {
-        Intent intent2 = new Intent(this, RecyclingImportance.class);
-        startActivity(intent2);
-
-        /*tContainer = findViewById(R.id.transitionContainer);
+        tContainer = findViewById(R.id.transitionContainer);
         txt = findViewById(R.id.testVisible);
         magic = findViewById(R.id.landfill);
 
@@ -38,7 +33,6 @@ public class RecyclingImportance extends AppCompatActivity {
                 visible = !visible;
                 txt.setVisibility(visible ? View.VISIBLE: View.GONE);
             }
-        });*/
-    }
+    });
 
-}
+    }}
