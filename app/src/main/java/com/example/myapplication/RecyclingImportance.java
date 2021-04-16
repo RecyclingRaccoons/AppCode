606ee807c850a9009c560d2fb83d7d3500412841
aswu308 <aswu@wpi.edu>
2021-04-16 00:52:07 -0400

    Version 2

--- app/src/main/java/com/example/myapplication/RecyclingImportance.java
+++ app/src/main/java/com/example/myapplication/RecyclingImportance.java
@@ -1,11 +1,12 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +14,15 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RecyclingImportance extends AppCompatActivity {
 
     ViewGroup tContainer;
-    TextView txt;
-    Button magic;
+    TextView landfill;
+    TextView incinerator;
+    TextView resources;
+    TextView energy;
+    Button landfillB;
+    Button incineratorB;
+    Button resourcesB;
+    Button energyB;
+    ImageButton exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +30,85 @@ public class RecyclingImportance extends AppCompatActivity {
         setContentView(R.layout.activity_recycling_importance);
 
         tContainer = findViewById(R.id.transitionContainer);
-        txt = findViewById(R.id.testVisible);
-        magic = findViewById(R.id.landfill);
+        landfill = findViewById(R.id.landfillInfo);
+        landfillB = findViewById(R.id.landfill);
+        exit = findViewById(R.id.exitBox);
 
-        magic.setOnClickListener(new View.OnClickListener() {
-            boolean visible;
+        landfillB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TransitionManager.beginDelayedTransition(tContainer);
-                visible = !visible;
-                txt.setVisibility(visible ? View.VISIBLE: View.GONE);
+                landfill.setVisibility(View.VISIBLE);
+                landfillB.setVisibility(View.GONE);
+                incineratorB.setVisibility(View.GONE);
+                resourcesB.setVisibility(View.GONE);
+                energyB.setVisibility(View.GONE);
+                exit.setVisibility(View.VISIBLE);
             }
     });
+
+        incinerator = findViewById(R.id.incineratorInfo);
+        incineratorB = findViewById(R.id.incinerator);
+
+        incineratorB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                incinerator.setVisibility(View.VISIBLE);
+                landfillB.setVisibility(View.GONE);
+                incineratorB.setVisibility(View.GONE);
+                resourcesB.setVisibility(View.GONE);
+                energyB.setVisibility(View.GONE);
+                exit.setVisibility(View.VISIBLE);
+            }
+        });
+
+        resources = findViewById(R.id.resourcesInfo);
+        resourcesB = findViewById(R.id.resources);
+
+        resourcesB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                resources.setVisibility(View.VISIBLE);
+                landfillB.setVisibility(View.GONE);
+                incineratorB.setVisibility(View.GONE);
+                resourcesB.setVisibility(View.GONE);
+                energyB.setVisibility(View.GONE);
+                exit.setVisibility(View.VISIBLE);
+            }
+        });
+
+        energy = findViewById(R.id.energyInfo);
+        energyB = findViewById(R.id.energy);
+
+        energyB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                energy.setVisibility(View.VISIBLE);
+                landfillB.setVisibility(View.GONE);
+                incineratorB.setVisibility(View.GONE);
+                resourcesB.setVisibility(View.GONE);
+                energyB.setVisibility(View.GONE);
+                exit.setVisibility(View.VISIBLE);
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                landfill.setVisibility(View.GONE);
+                incinerator.setVisibility(View.GONE);
+                resources.setVisibility(View.GONE);
+                energy.setVisibility(View.GONE);
+                landfillB.setVisibility(View.VISIBLE);
+                incineratorB.setVisibility(View.VISIBLE);
+                resourcesB.setVisibility(View.VISIBLE);
+                energyB.setVisibility(View.VISIBLE);
+                exit.setVisibility(View.GONE);
+            }
+        });
 
     }}
