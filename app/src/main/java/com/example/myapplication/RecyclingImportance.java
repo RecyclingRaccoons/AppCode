d2a17738d202afdbbd0d7c3e302a0c979dca16da
aswu308 <aswu@wpi.edu>
2021-04-20 13:45:49 -0400

    Version 2

--- app/src/main/java/com/example/myapplication/RecyclingImportance.java
+++ app/src/main/java/com/example/myapplication/RecyclingImportance.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RecyclingImportance extends AppCompatActivity {
 
     ViewGroup tContainer;
+
     TextView landfill;
     TextView incinerator;
     TextView resources;
@@ -24,6 +25,24 @@ public class RecyclingImportance extends AppCompatActivity {
     Button resourcesB;
     Button energyB;
     ImageButton exit;
+
+    TextView circular;
+    TextView jobs;
+    Button circularB;
+    Button jobsB;
+    ImageButton exit2;
+
+    TextView collection;
+    TextView cleaning;
+    TextView manufacturing;
+    TextView newProduct;
+    Button collectionB;
+    Button cleaningB;
+    Button manufacturingB;
+    Button newProductB;
+    ImageButton exit3;
+
+
     // Hidden text tutorial: https://www.youtube.com/watch?v=qj3kKB71u84
 
     @Override
@@ -110,6 +129,125 @@ public class RecyclingImportance extends AppCompatActivity {
                 resourcesB.setVisibility(View.VISIBLE);
                 energyB.setVisibility(View.VISIBLE);
                 exit.setVisibility(View.GONE);
+            }
+        });
+
+        circular = findViewById(R.id.circularEconInfo);
+        circularB = findViewById(R.id.circularEcon);
+        exit2 = findViewById(R.id.exitBox2);
+
+        circularB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                circular.setVisibility(View.VISIBLE);
+                circularB.setVisibility(View.GONE);
+                jobsB.setVisibility(View.GONE);
+                exit2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        jobs = findViewById(R.id.jobsInfo);
+        jobsB = findViewById(R.id.jobs);
+
+        jobsB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                jobs.setVisibility(View.VISIBLE);
+                circularB.setVisibility(View.GONE);
+                jobsB.setVisibility(View.GONE);
+                exit2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        exit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                circular.setVisibility(View.GONE);
+                jobs.setVisibility(View.GONE);
+                circularB.setVisibility(View.VISIBLE);
+                jobsB.setVisibility(View.VISIBLE);
+                exit2.setVisibility(View.GONE);
+            }
+        });
+
+        collection = findViewById(R.id.collectionInfo);
+        collectionB = findViewById(R.id.collection);
+        cleaning = findViewById(R.id.cleaningInfo);
+        cleaningB = findViewById(R.id.cleaning);
+        manufacturing = findViewById(R.id.manufacturingInfo);
+        manufacturingB = findViewById(R.id.manufacturing);
+        newProduct = findViewById(R.id.newProductInfo);
+        newProductB = findViewById(R.id.newProduct);
+        exit3 = findViewById(R.id.exitBox3);
+
+        collectionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                collection.setVisibility(View.VISIBLE);
+                collectionB.setVisibility(View.GONE);
+                cleaningB.setVisibility(View.GONE);
+                manufacturingB.setVisibility(View.GONE);
+                newProductB.setVisibility(View.GONE);
+                exit3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        cleaningB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                cleaning.setVisibility(View.VISIBLE);
+                collectionB.setVisibility(View.GONE);
+                cleaningB.setVisibility(View.GONE);
+                manufacturingB.setVisibility(View.GONE);
+                newProductB.setVisibility(View.GONE);
+                exit3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        manufacturingB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                manufacturing.setVisibility(View.VISIBLE);
+                collectionB.setVisibility(View.GONE);
+                cleaningB.setVisibility(View.GONE);
+                manufacturingB.setVisibility(View.GONE);
+                newProductB.setVisibility(View.GONE);
+                exit3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        newProductB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                newProduct.setVisibility(View.VISIBLE);
+                collectionB.setVisibility(View.GONE);
+                cleaningB.setVisibility(View.GONE);
+                manufacturingB.setVisibility(View.GONE);
+                newProductB.setVisibility(View.GONE);
+                exit3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        exit3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(tContainer);
+                collection.setVisibility(View.GONE);
+                cleaning.setVisibility(View.GONE);
+                manufacturing.setVisibility(View.GONE);
+                newProduct.setVisibility(View.GONE);
+                collectionB.setVisibility(View.VISIBLE);
+                cleaningB.setVisibility(View.VISIBLE);
+                manufacturingB.setVisibility(View.VISIBLE);
+                newProductB.setVisibility(View.VISIBLE);
+                exit3.setVisibility(View.GONE);
             }
         });
 
