696c929a29cae6e6dbf657e7229ac90409ac2bca
amankad22 <amankad@wpi.edu>
2021-04-20 13:22:54 -0400

    paper page

--- app/src/main/java/com/example/myapplication/PaperPage.java
+++ app/src/main/java/com/example/myapplication/PaperPage.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PaperPage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class PaperPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paper_page);
+    }
+
+    public void toHomePage(View v) {
+        Intent toHome = new Intent(this, MainActivity.class);
+        startActivity(toHome);
+    }
+
+    public void recyclingMainPage(View v) {
+        Intent toCategories = new Intent(this, RecyclingCategories.class);
+        startActivity(toCategories);
     }
 }
