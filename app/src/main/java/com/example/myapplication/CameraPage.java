39b6b3d808044f4d666917950ed2c9593ead4ec0
aswu308 <aswu@wpi.edu>
2021-04-29 08:52:34 -0400

    Restarting Camera Code

--- app/src/main/java/com/example/myapplication/CameraPage.java
+++ app/src/main/java/com/example/myapplication/CameraPage.java
@@ -1,48 +1,35 @@
 package com.example.myapplication;
 
 
-import android.content.Context;
+
+import android.Manifest;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class CameraPage extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageButton camera;
+    ImageButton gallery;
     ImageView displayer;
 
+    private static final int REQUEST_CODE = 101;
+
     //Follow camera tutorial:
-    // https://www.geeksforgeeks.org/android-how-to-open-camera-through-intent-and-display-captured-image/
-    //https://developer.android.com/training/camera/photobasics#TaskPath
+    //https://www.youtube.com/watch?v=zeI0M9PtOBA
+    //https://www.youtube.com/watch?v=s1aOlr3vbbk
 
-    String currentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,32 +38,38 @@ public class CameraPage extends AppCompatActivity {
 
         camera = findViewById(R.id.cameraOption);
         displayer = findViewById(R.id.imgDisplayer);
+        gallery = findViewById(R.id.imgOption);
 
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(camera_intent, REQUEST_IMAGE_CAPTURE);
+                askCameraPermissions();
+                Intent cam = new Intent();
+                cam.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(cam);
+            }
+        });
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            displayer.setImageBitmap(imageBitmap);
+    private void askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, REQUEST_CODE);
         }
-        Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
-        int duration = Toast.LENGTH_SHORT;
+        else {
+            //openCamera();
+        }
+    }
 
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-
-        Intent img_accept = new Intent(this, ImageAcceptance.class);
-        startActivity(img_accept);
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public void toHomePage (View v){
@@ -94,4 +87,13 @@ public class CameraPage extends AppCompatActivity {
         startActivity(toCamera);
     }
 
+    // Context context = getApplicationContext();
+    //        CharSequence text = "Hello toast!";
+    //        int duration = Toast.LENGTH_SHORT;
+    //
+    //        Toast toast = Toast.makeText(context, text, duration);
+    //        toast.show();
+    //
+    //        Intent img_accept = new Intent(this, ImageAcceptance.class);
+    //        startActivity(img_accept);
 }
