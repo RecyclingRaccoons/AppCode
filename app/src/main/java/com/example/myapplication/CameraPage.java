b4bdf5642b7e5db42e55d19808fd70a1bbc6357c
aswu308 <aswu@wpi.edu>
2021-05-03 12:47:06 -0400

    Camera is Operational

--- app/src/main/java/com/example/myapplication/CameraPage.java
+++ app/src/main/java/com/example/myapplication/CameraPage.java
@@ -5,14 +5,17 @@ package com.example.myapplication;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -23,8 +26,11 @@ public class CameraPage extends AppCompatActivity {
     ImageButton camera;
     ImageButton gallery;
     ImageView displayer;
+    Button cancel;
+    Button accept;
 
-    private static final int REQUEST_CODE = 101;
+    private static final int PERM_CODE = 101;
+    private static final int REQUEST_CODE = 102;
 
     //Follow camera tutorial:
     //https://www.youtube.com/watch?v=zeI0M9PtOBA
@@ -39,14 +45,13 @@ public class CameraPage extends AppCompatActivity {
         camera = findViewById(R.id.cameraOption);
         displayer = findViewById(R.id.imgDisplayer);
         gallery = findViewById(R.id.imgOption);
+        cancel = findViewById(R.id.cancelButton);
+        accept = findViewById(R.id.nextButton);
 
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 askCameraPermissions();
-                Intent cam = new Intent();
-                cam.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(cam);
             }
         });
 
@@ -56,20 +61,64 @@ public class CameraPage extends AppCompatActivity {
 
             }
         });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.setVisibility(View.VISIBLE);
+                gallery.setVisibility(View.VISIBLE);
+                displayer.setVisibility(View.GONE);
+                cancel.setVisibility(View.GONE);
+                accept.setVisibility(View.GONE);
+            }
+        });
+
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void askCameraPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, PERM_CODE);
         }
         else {
-            //openCamera();
+            openCamera();
         }
     }
 
     @Override
     public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+       if (requestCode == PERM_CODE) {
+           if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+           }
+           else {
+               Toast.makeText(this, "Camera Permission is Required to use the camera", Toast.LENGTH_SHORT).show();
+           }
+       }
+    }
+
+    private void openCamera() {
+        Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cam, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            displayer.setImageBitmap(image);
+            camera.setVisibility(View.GONE);
+            gallery.setVisibility(View.GONE);
+            displayer.setVisibility(View.VISIBLE);
+            cancel.setVisibility(View.VISIBLE);
+            accept.setVisibility(View.VISIBLE);
+        }
     }
 
     public void toHomePage (View v){
@@ -86,14 +135,4 @@ public class CameraPage extends AppCompatActivity {
         Intent toCamera = new Intent(this, CameraPage.class);
         startActivity(toCamera);
     }
-
-    // Context context = getApplicationContext();
-    //        CharSequence text = "Hello toast!";
-    //        int duration = Toast.LENGTH_SHORT;
-    //
-    //        Toast toast = Toast.makeText(context, text, duration);
-    //        toast.show();
-    //
-    //        Intent img_accept = new Intent(this, ImageAcceptance.class);
-    //        startActivity(img_accept);
 }
